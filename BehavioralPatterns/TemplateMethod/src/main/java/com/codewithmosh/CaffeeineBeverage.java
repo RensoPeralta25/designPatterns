17ee9112cd3148e8248c558fb7ee6dbdea257b97
Renso Peralta <ripu0001@ce.pucmm.edu.do>
2026-04-06 19:03:12 -0400

    feat: template method implementado

---
+++ BehavioralPatterns/TemplateMethod/src/main/java/com/codewithmosh/CaffeeineBeverage.java
@@ -0,0 +1,23 @@
+package com.codewithmosh;
+
+abstract class CaffeeineBeverage {
+
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    abstract void brew();
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+}
+
---
+++ BehavioralPatterns/TemplateMethod/src/main/java/com/codewithmosh/Coffee.java
@@ -0,0 +1,11 @@
+package com.codewithmosh;
+
+public class Coffee extends CaffeeineBeverage {
+    void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
+}
