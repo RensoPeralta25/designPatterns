944ac3b7cbc193774048e4812bbc94822243dce8
jmcompres <jmca0001@ce.pucmm.edu.do>
2026-04-06 10:43:36 -0400

    Ejemplo Singleton

--- CreationalPatterns/Singleton/src/main/java/edu/pucmm/eict/Main.java
+++ CreationalPatterns/Singleton/src/main/java/edu/pucmm/eict/Main.java
@@ -4,14 +4,14 @@ package edu.pucmm.eict;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        AppConfig a = AppConfig.getInstance();
+        AppConfig b = AppConfig.getInstance();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        System.out.println(a == b);
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+
+        b.setTheme("dark");
+        System.out.println(a.getTheme());
     }
 }
